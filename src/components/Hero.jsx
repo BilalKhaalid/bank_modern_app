726df35d8cb43674d30c494d3ca4e4bb3cb9ecb6
bilalkhaalid <bilall1432@gmail.com>
2023-05-08 18:49:51 +0500

    Discount section of hero component is completed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import styles from "../style";
+import { discount, robot } from "../assets";
+const Hero = () => (
+  <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
+    {/* creating a div for logo image of discount section and the content of discount section */}
 
-function Hero() {
-  return (
-    <div>Hero</div>
-  )
-}
+    <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16`}>
+      {/* this div contains the discount logo image and its content which is placed by tailwind classes */}
+      <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2">
+        {/* Image contains discount logo */}
+        <img src={discount} alt="discount" className="w-[32px] h-[32px]" />
 
-export default Hero
+        {/* In this paragraph tag it contains the content of discount section. we are using spans to and simple text in paragraph to differentiate the color by a variable. */}
+        <p className={`${styles.paragraph} ml-2`}>
+          <span className="text-white">20%</span> Discount For
+          <span className="text-white">1 Month</span> Account
+        </p>
+      </div>
+    </div>
+  </section>
+);
+
+export default Hero;
