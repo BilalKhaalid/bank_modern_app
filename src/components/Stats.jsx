b95b6225fee50d91084b5385b63208d93c417725
BilalKhaalid <bilall1432@gmail.com>
2023-05-09 11:27:17 +0500

    Stats completed with responsive design

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
+import styles from "../style";
+import { stats } from "../constants";
 
-function Stats() {
-  return (
-    <div>Stats</div>
-  )
-}
+const Stats = () => (
+  <section className={`${styles.flexCenter} flex-wrap flex-row sm:mb-20 mb-6`}>
+    {/* mapping through the stats object */}
+    {stats.map((stat) => (
+      //! giving key to the div for 1 to 3 id number of the item
+      <div
+        key={stat.id}
+        className="flex flex-1 justify-start items-center flex-row m-3 "
+      >
+        <h4
+          //! giving value of stat for providing value of stats object
+          className="text-white font-poppins font-semibold xs:text-[40px] text-[30px] xs:leading-[53px] leading-[43px]  
+      "
+        >
+          {stat.value}
+        </h4>
+        <p
+          //! giving title of stat for providing title of stats object
 
-export default Stats
+          className="text-gradient font-poppins font-normal xs:text-[20px] text-[15px] xs:leading-[26px] leading-[21px] uppercase ml-3 "
+        >
+          {stat.title}
+        </p>
+      </div>
+    ))}
+  </section>
+);
+
+export default Stats;
