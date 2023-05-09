715da1ebae695a7fa33345d24606c67852c9d18a
bilalkhaalid <bilall1432@gmail.com>
2023-05-09 09:56:49 +0500

    Get started button completed

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
+import { arrowUp } from "../assets";
+import styles from "../style";
 
 function GetStarted() {
   return (
-    <div>GetStarted</div>
-  )
+    // ! creating a div for button and making it round and gradient which will look nice with our theme
+    <div
+      className={`${styles.flexCenter} w-[140px] h-[140px] bg-blue-gradient rounded-full p-[2px] cursor-pointer`}
+    >
+      //! creating another div for inner area of the rounded button which will give it a very nice gradient border
+      <div
+        className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-full rounded-full`}
+      >
+        //! creating this div for containing the content of the button and icon of the arrow of get started
+        <div className={`${styles.flexStart} flex-row`}>
+          <p className="font-poppins font-medium text-[18px] leading-[23px] mr-2">
+            <span className="text-gradient">Get</span>
+          </p>
+          <img
+            src={arrowUp}
+            alt="arrow-up"
+            className="w-[23px] h-[23px] object-contain"
+          />
+        </div>
+        {/* creating a paragraph tag to hold second part of the content of get started button */}
+        <p className="font-poppins font-medium text-[18px] leading-[23px]">
+          <span className="text-gradient">Started</span>
+        </p>
+      </div>
+    </div>
+  );
 }
 
-export default GetStarted
+export default GetStarted;
