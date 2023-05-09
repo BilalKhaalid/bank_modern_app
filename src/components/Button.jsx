4344c24ac69c880bb5ee414926c947f91aba611f
BilalKhaalid <bilall1432@gmail.com>
2023-05-09 11:58:50 +0500

    Comments added to button comp of business section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 const Button = ({ styles }) => (
   <button
+  //! creating this button element and adding the props directly in the className
     className={`py-4 px-6 bg-blue-gradient font-poppins text-[18px] text-primary outline-none rounded-lg ${styles} `}
     type="button"
   >
