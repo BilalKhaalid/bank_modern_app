46f9580901755dda88be8e9257d222e265ebb61f
bilalkhaalid <bilall1432@gmail.com>
2023-05-09 10:01:31 +0500

    Comments fixed

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -4,16 +4,18 @@ import styles from "../style";
 
 function GetStarted() {
   return (
-    // ! creating a div for button and making it round and gradient which will look nice with our theme
     <div
+      // ! creating a div for button and making it round and gradient which will look nice with our theme
       className={`${styles.flexCenter} w-[140px] h-[140px] bg-blue-gradient rounded-full p-[2px] cursor-pointer`}
     >
-      //! creating another div for inner area of the rounded button which will give it a very nice gradient border
       <div
+        //! creating another div for inner area of the rounded button which will give it a very nice gradient border
         className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-full rounded-full`}
       >
-        //! creating this div for containing the content of the button and icon of the arrow of get started
-        <div className={`${styles.flexStart} flex-row`}>
+        <div
+          //! creating this div for containing the content of the button and icon of the arrow of get started
+          className={`${styles.flexStart} flex-row`}
+        >
           <p className="font-poppins font-medium text-[18px] leading-[23px] mr-2">
             <span className="text-gradient">Get</span>
           </p>
